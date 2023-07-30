1e4623ad875fe02f43799b1d04423a01ae5d2cf5
esperoj <esperoj@esperoj.eu.org>
2023-07-30 14:48:00 +0700

    update

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 
 # Setup ssh
 mkdir -p ~/.ssh
-curl -sSfl "https://codeberg.org/esperoj/dotfiles/raw/branch/main/private_dot_ssh/encrypted_private_id_ed25519.asc" | gpg --passphrase "${ENCRYPTION_PASSPHRASE}" --batch -o ~/.ssh/id_ed25519 -d
+curl -sSfl "https://codeberg.org/esperoj/dotfiles/raw/branch/main/private_dot_ssh/encrypted_private_id_ed25519.asc" | gpg --passphrase "${ENCRYPTION_PASSPHRASE}" --batch -d >~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 curl -sSfl "https://codeberg.org/esperoj/dotfiles/raw/branch/main/private_dot_ssh/private_known_hosts" >~/.ssh/known_hosts
 
