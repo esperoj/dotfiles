7af52a96a54f6d97bb2e7e9ab133d9f60b95c76a
esperoj <esperoj@esperoj.eu.org>
2023-08-04 17:23:50 +0700

    update

--- private_scripts/private_executable_setup.sh
+++ private_scripts/private_executable_setup.sh
@@ -48,12 +48,8 @@ if [[ $(uname -o) = *Linux* ]]; then
   chezmoi init --apply "${src}"
 	source .profile
 	pkg-install.sh BASE apt-get install -qqy --no-install-recommends 7zip jq parallel python3 sqlite3 
-	echo 'aria2 aria2c
-rclone
-restic' | xargs -I {} bash -c 'pkg-install.sh NET asdf_install {}'
-	echo 'nodejs node
-shfmt
-shellcheck' | xargs -I {} bash -c 'pkg-install.sh DEV asdf_install {}'
+	echo 'aria2 aria2c,rclone,restic' | tr "," "\n" | xargs -I {} bash -c 'pkg-install.sh NET asdf_install {}'
+	echo 'nodejs node,shfmt,shellcheck' | tr "," "\n" | xargs -I {} bash -c 'pkg-install.sh DEV asdf_install {}'
 	pkg-install.sh ALL apt-get install -qqy --no-install-recommends ffmpeg yt-dlp
 	pkg-install.sh INTERACTIVE apt-get install -qqy --no-install-recommends vim tmux mosh zsh fzf
 	pkg-install.sh INTERACTIVE install_oh_my_zsh
