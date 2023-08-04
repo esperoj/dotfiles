56d50f7e73fce8ea18c6bf3a6369f12831eb2834
esperoj <esperoj@esperoj.eu.org>
2023-08-04 07:04:13 +0700

    update

--- private_scripts/private_executable_setup.sh
+++ private_scripts/private_executable_setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -Eeuo pipefail
+
 cd "${HOME}"
 mkdir -p ${HOME}/{.local/,}bin
-OS=$(uname -o)
-init_chezmoi() {
-	chezmoi init --apply --depth=1 --force --purge https://codeberg.org/esperoj/dotfiles.git
-}
+
 asdf_install() {
 	set -- "$@"
 	[[ $(command -v ${2-$1}) ]] && echo "The packages $1 is installed" && return
@@ -17,21 +15,29 @@ install_oh_my_zsh() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
 export -f asdf_install install_oh_my_zsh
+
 if [[ $OS = Android ]]; then
 	apt update -qy
 	apt install -qy 7zip aria2 chezmoi curl git jq mosh parallel rclone restic shfmt sqlite tmux vim wget gnupg zsh fzf
   install_oh_my_zsh
-	init_chezmoi
+	chezmoi init --apply --depth=1 --force --purge https://codeberg.org/esperoj/dotfiles.git
   chezmoi init --apply --ssh git@codeberg.org:esperoj/dotfiles.git
 	exit
 fi
+
+# Install chezmoi and init the environment
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
+chezmoi init --one-shot https://codeberg.org/esperoj/dotfiles.git
+source .profile
+
+# Install packages
 apt update -qy
-apt-get install -qy --no-install-recommends 7zip curl dirmngr git gnupg jq parallel python3 sqlite3 wget
+pkg-install.sh BASE apt-get install -qy --no-install-recommends 7zip curl dirmngr git gnupg jq parallel python3 sqlite3 wget
+
 git clone --quiet --depth=1 https://github.com/asdf-vm/asdf.git ~/.asdf --branch master
 . "$HOME/.asdf/asdf.sh"
+
 echo 'chezmoi' | xargs -I {} bash -c 'pkg-install.sh BASE asdf_install {}'
-init_chezmoi
-source .profile
 echo 'aria2 aria2c
 rclone
 restic' | xargs -I {} bash -c 'pkg-install.sh NET asdf_install {}'
@@ -41,5 +47,9 @@ shellcheck' | xargs -I {} bash -c 'pkg-install.sh DEV asdf_install {}'
 pkg-install.sh ALL apt-get install -qy --no-install-recommends ffmpeg yt-dlp
 pkg-install.sh INTERACTIVE apt-get install -qy --no-install-recommends vim tmux mosh zsh fzf
 pkg-install.sh INTERACTIVE install_oh_my_zsh
+
+# Reinit with ssh
 chezmoi init --apply --ssh git@codeberg.org:esperoj/dotfiles.git
+
+# Making links
 ln -s $(command -v 7zz) "${HOME}/.local/bin/7z"
