00a1b558e2eb057e7b1885137075fb89884932a3
esperoj <esperoj@esperoj.eu.org>
2023-08-04 16:52:07 +0700

    update

--- private_scripts/private_executable_setup.sh
+++ private_scripts/private_executable_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -Eeuo pipefail
-
+set -Exeuo pipefail
+src=${1:-'git@codeberg.org:esperoj/dotfiles.git'}
 cd "${HOME}"
-mkdir -p ${HOME}/{.local/,}bin
+mkdir -p ${HOME}/.local/{bin,share,lib,lib64}
 mkdir -p "${HOME}/.ssh/sockets"
 
 asdf_install() {
@@ -13,10 +13,17 @@ asdf_install() {
 	asdf global $1 latest
 }
 install_oh_my_zsh() {
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 }
 export -f asdf_install install_oh_my_zsh
 
+# Setup ssh
+mkdir -p ~/.ssh
+curl -sSfl "https://codeberg.org/esperoj/dotfiles/raw/branch/main/private_dot_ssh/encrypted_private_id_ed25519.asc" | gpg --passphrase "${ENCRYPTION_PASSPHRASE}" --batch -d >~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+curl -sSfl "https://codeberg.org/esperoj/dotfiles/raw/branch/main/private_dot_ssh/private_known_hosts" >~/.ssh/known_hosts
+
+#Install packages on android
 if [[ $(uname -o) = Android ]]; then
 	apt-get update -qqy
 	apt-get install -qqy 7zip aria2 chezmoi curl git jq mosh parallel rclone restic shfmt sqlite tmux vim wget gnupg zsh fzf openssh-client
@@ -31,13 +38,16 @@ if [[ $(uname -o) = *Linux* ]]; then
 	apt-get install -qqy --no-install-recommends curl gnupg git unzip bzip2 wget dirmngr
 
 	# Install chezmoi and init the environment
-	sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
-  git clone --depth=1 --quiet https://codeberg.org/esperoj/dotfiles.git ~/dotfiles
-	$HOME/.local/bin/chezmoi init --one-shot ~/dotfiles
+  # Install asdf
 	git clone --quiet --depth=1 https://github.com/asdf-vm/asdf.git ~/.asdf --branch master
+  . "$HOME/.asdf/asdf.sh"
+  # Install chezmoi
+	echo 'chezmoi' | xargs -I {} bash -c 'pkg-install.sh BASE asdf_install {}'
+  # If the src is not a local path, then clone it and then change src to a local path
+  [[ ! -d ${src} ]] && git clone --depth=1 --quiet ${src} .local/share/chezmoi  && src="${HOME}/.local/share/chezmoi"
+  chezmoi init --apply ${src@Q}
 	source .profile
 	pkg-install.sh BASE apt-get install -qqy --no-install-recommends 7zip jq parallel python3 sqlite3 openssh-client
-	echo 'chezmoi' | xargs -I {} bash -c 'pkg-install.sh BASE asdf_install {}'
 	echo 'aria2 aria2c
 rclone
 restic' | xargs -I {} bash -c 'pkg-install.sh NET asdf_install {}'
@@ -47,10 +57,6 @@ shellcheck' | xargs -I {} bash -c 'pkg-install.sh DEV asdf_install {}'
 	pkg-install.sh ALL apt-get install -qqy --no-install-recommends ffmpeg yt-dlp
 	pkg-install.sh INTERACTIVE apt-get install -qqy --no-install-recommends vim tmux mosh zsh fzf
 	pkg-install.sh INTERACTIVE install_oh_my_zsh
-
-	# Reinit with ssh
-	chezmoi init --apply --ssh git@codeberg.org:esperoj/dotfiles.git
-  rm -rf dotfiles
 fi
 
 ln -s $(command -v 7zz) "${HOME}/.local/bin/7z"
