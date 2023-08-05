ea3fb2090a600e1bdec755e61cecd8eb4c7f4ce6
esperoj <esperoj@esperoj.eu.org>
2023-08-05 20:28:27 +0700

    update

---
+++ scripts/info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -Eeuxo pipefail
+lsb_release -a
+uname -a
+chezmoi status
+rclone listremotes
+restic stats
+python --version
+node --version
+ssh segfault "uname -a; lsb_release -a"
+ssh envs "uname -a; lsb_release -a"
+ssh serv00 "uname -a"
+ssh ct8 "uname -a"
