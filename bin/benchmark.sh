8ff96f2d253c39327eb6473120c5b80a924f9aab
esperoj <esperoj@esperoj.eu.org>
2023-08-24 13:42:43 +0700

    add benchmark.sh

---
+++ bin/benchmark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -Eeuxo pipefail
+curl -fLsS "https://ipwho.de"
+df -h
+# Test network speed
+curl -fLsS "https://raw.githubusercontent.com/su-haris/simple-network-speedtest/master/speed.sh" | bash
+curl -fLsS "https://yabs.sh" | bash -s -- -is "https://www.vpsbenchmarks.com/yabs/upload"
