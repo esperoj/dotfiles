b6a6e9c90ad896ad85a74075a6da3223f8abf9fe
esperoj <esperoj@esperoj.eu.org>
2023-08-24 12:25:25 +0700

    add backup to drone.yml

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -3,7 +3,8 @@ set -Eueo pipefail
 # Install chezmoi
 path="${HOME}/.local/bin"
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "${path}"
-# Init chezmoi
-"${path}/chezmoi" init https://codeberg.org/esperoj/dotfiles.git
+# Clone dotfiles
+mkdir -p "${HOME}/.local/share/chezmoi"
 cd "${HOME}/.local/share/chezmoi"
+git clone --depth=1 https://codeberg.org/esperoj/dotfiles.git .
 git remote set-url origin git@codeberg.org:esperoj/dotfiles.git
