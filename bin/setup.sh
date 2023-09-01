a499b53221045554483a574a92c3577df3055eed
esperoj <esperoj@esperoj.eu.org>
2023-09-01 10:43:25 +0700

    install binary using pkg-install.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 export PATH="${HOME}/bin:${HOME}/.local/bin:${PATH}"
 cmds=$(echo '
   install_7zip
-  # install_kopia
+  #install_kopia
   install_oh_my_zsh
   install_packages
   install_rclone
