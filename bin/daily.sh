ebb3322832b0365572637dd057ca3b642f1e2d91
esperoj <esperoj@esperoj.eu.org>
2023-12-03 14:41:47 +0700

    update rclone.conf

--- bin/daily.sh
+++ bin/daily.sh
@@ -8,15 +8,10 @@ keep_pcloud_active() {
   rm ping.txt
 }
 
-fresh_feeds() {
-  curl -fsS -m 300 --retry 5 "https://frss.adminforge.de/i/?c=feed&a=actualize&force=1&user=esperoj&token=${MY_UUID}&ajax=1"
-}
-
 export -f keep_pcloud_active fresh_feeds
 
 parallel --keep-order -vj0 {} <<-EOL
   ssh ct8 "devil info account"
   ssh serv00 "devil info account"
   keep_pcloud_active
-  fresh_feeds
 EOL
