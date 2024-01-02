03028ecbc13ab16a1e275efcee3154b4c615b443
esperoj <esperoj@esperoj.eu.org>
2024-01-02 18:35:03 +0700

    use pcloud instead of koofr

--- bin/daily-backup.sh
+++ bin/daily-backup.sh
@@ -11,7 +11,7 @@ backup_phone() {
 	cd "/storage/emulated/0/"
 	echo "- joplin/" >>"${RCLONE_FILTER_FROM}"
 	rclone sync ./workspace workspace:
-	rclone sync ./music koofr:music
+	rclone sync ./music pcloud:Music
 }
 
 backup_segfault() {
