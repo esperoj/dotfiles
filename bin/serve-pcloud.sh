15bb08d64e0d4395827b2a7feb13558510aacc75
esperoj <esperoj@esperoj.eu.org>
2024-03-13 12:40:42 +0100

    update

--- bin/serve-pcloud.sh
+++ bin/serve-pcloud.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-screen -dmS pcloud-public sh -c "
-  rclone serve http \
-    --addr localhost:20711 \
-    --dir-cache-time 0s \
-    pcloud:public
-"
+. ~/.profile
+rclone serve http \
+  --addr "[::]:${PORT}" \
+  --dir-cache-time 0s \
+  pcloud:public
