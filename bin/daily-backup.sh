c1722e8665a871f775130112ae1a9066837c50c0
esperoj <esperoj@esperoj.eu.org>
2024-03-18 15:05:35 +0000

    Daily call daily update

--- bin/daily-backup.sh
+++ bin/daily-backup.sh
@@ -27,16 +27,13 @@ export -f backup_linkwarden backup_seatable
 
 backup_container() {
   parallel --keep-order -vj0 {} <<-EOL
-  ssh envs bash -s <<<'~/bin/daily-backup.sh'
+  ssh envs bash -s <<<'. ~/.profile && chezmoi update --force --no-tty && . ~/.profile && daily-backup.sh'
   rclone sync --transfers 8 pcloud: nch:
 EOL
 }
 
 backup_segfault() {
   cd ~
-  source ./.profile
-  chezmoi update --force --no-tty
-  source ./.profile
   pipx upgrade esperoj
   parallel --keep-order -vj0 {} <<-EOL
   backup_linkwarden
