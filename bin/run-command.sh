8ece75ddf85bcf99cb1be205975d1575c981b0c6
esperoj <esperoj@esperoj.eu.org>
2024-05-23 06:52:48 +0000

    update gitlab ci

--- bin/run-command.sh
+++ bin/run-command.sh
@@ -27,36 +27,50 @@ if [ -z "${command}" ]; then
   usage
 fi
 
-content=$(
-  jq -n \
-    --arg command "${command}" \
-    '{
+case "${host}" in
+codeberg | cezeri)
+  content=$(
+    jq -n \
+      --arg command "${command}" \
+      '{
        "branch": "main",
        "variables": {
          "WORKFLOW": "run-command",
          "COMMAND": $command
        }
      }'
-)
+  )
 
-case "${host}" in
-codeberg)
-  server=ci.codeberg.org
-  repo_id=12554
-  token="${WOODPECKER_TOKEN}"
+  case "${host}" in
+  codeberg)
+    server=ci.codeberg.org
+    repo_id=12554
+    token="${WOODPECKER_TOKEN}"
+    ;;
+  cezeri)
+    server=build.cezeri.tech
+    repo_id=9
+    token="${CEZERI_WOODPECKER_TOKEN}"
+    ;;
+  esac
+  result=$(curl -s -X POST "https://${server}/api/repos/${repo_id}/pipelines" \
+    -H "Authorization: Bearer ${token}" \
+    -H "Content-type: application/json" \
+    -d "${content}")
+
+  number=$(echo "${result}" | jq .number)
+
+  echo "https://${server}/repos/${repo_id}/pipeline/${number}"
   ;;
-cezeri)
-  server=build.cezeri.tech
-  repo_id=9
-  token="${CEZERI_WOODPECKER_TOKEN}"
+framagit)
+  result=$(curl -sX POST \
+    --fail \
+    -F token="${FRAMAGIT_DOTFILES_TRIGGER_TOKEN}" \
+    -F "ref=main" \
+    -F "variables[WORKFLOW]=run-command" \
+    -F "variables[COMMAND]=${command}" \
+    https://framagit.org/api/v4/projects/107814/trigger/pipeline |
+    jq .web_url)
+  echo "${result}"
   ;;
 esac
-
-result=$(curl -s -X POST "https://${server}/api/repos/${repo_id}/pipelines" \
-  -H "Authorization: Bearer ${token}" \
-  -H "Content-type: application/json" \
-  -d "${content}")
-
-number=$(echo "${result}" | jq .number)
-
-echo "https://${server}/repos/${repo_id}/pipeline/${number}"
