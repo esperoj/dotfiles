6145b2a12866ec77184ea51c4ca9df3fab87137e
esperoj <esperoj@esperoj.eu.org>
2024-10-28 13:59:32 +0700

    update [skip ci]

--- bin/benchmark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -Eexo pipefail
-curl -sL nws.sh | bash
-curl -fLsS "https://yabs.sh" | bash -s -- -is "https://www.vpsbenchmarks.com/yabs/upload"
-7z b
--- bin/transfer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-FILENAME=$(basename "$1")
-
-rclone copy -P "$1" "cache:"
-rclone link "cache:${FILENAME}"
