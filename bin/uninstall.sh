55840f2b14f1076664fb83b053539981a642507a
esperoj <esperoj@esperoj.eu.org>
2025-06-25 21:49:41 +0700

    update

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-  cd "$HOME/.local/bin"
-  rm -rf "$HOME/.local/opt/$1"
-  find -L . -name . -o -type d -prune -o -type l -exec rm {} +
+cd "$HOME/.local/bin"
+rm -rf "$HOME/.local/opt/$1"
+find -L . -name . -o -type d -prune -o -type l -exec rm {} +
